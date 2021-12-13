2bb51e055453addd850abd780d20d7c3aba80ba0
hoa2000kxpt <hoa2000kxpt@gmail.com>
2021-12-13 23:36:59 +0700

    new updates

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,33 @@
+from django.db.models import query
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
-def starting_page(request):
-    last_posts = Post.objects.all().order_by("-date")[:3] # Get 3 current post 
-    # last_posts = sorted_posts[-3:]
-    return render(request, 'blog/index.html', {
-        "posts": last_posts
-    })
+class StartingPageView(ListView):
+    template_name = 'blog/index.html'
+    model = Post
+    ordering = ['-date']
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
 
 
-def posts(request):
-    all_posts = Post.objects.all().order_by("-date")
-    return render(request, 'blog/all-posts.html', {
-        "all_posts": all_posts
-    })
+class AllPostView(ListView):
+    template_name = 'blog/all-posts.html'
+    model = Post
+    ordering = ['-date']
+    context_object_name = 'all_posts'
 
 
-def post_detail(request, slug):  # Use slug for dynamic URL
-    defined_post = get_object_or_404(Post, slug=slug)
-    return render(request, 'blog/post-detail.html', {
-        "post": defined_post,
-        "post_tags": defined_post.tags.all()
-    })
+class PostDetailView(DetailView):
+    template_name = 'blog/post-detail.html'
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post_tags'] = self.object.tags.all()
+        return context
