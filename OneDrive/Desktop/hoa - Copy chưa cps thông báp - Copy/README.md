f56ecc313d5e20b774acf0c7d5a02820ffbd9e53
0348xxyyzz <94062685+0348xxyyzz@users.noreply.github.com>
2021-12-28 21:41:33 +0700

    update

---
+++ OneDrive/Desktop/hoa - Copy chưa cps thông báp - Copy/README.md
@@ -0,0 +1,23 @@
+# Welcome to CompanyBlog!
+
+## This is our Project Guideline. Please read this document carefully in order to run our project.
+
+### 1. Project Overview
+This blog will be directed to IFI Solution, Top 10 IT enterprises in Vietnam 2020. We also took a look at the company's website, and found that fun activities or company bondings' news are rarely mentioned here. Therefore, the expected results of my group's project is a suggestion to help the company promote the company's culture and working environment with a separate "Blogs" section with a "News" section.
+
+### 2. Delivarables
+Besides Project Guideline and Project Code, some documents relating to the project are listed below:
+- Database Design: https://drive.google.com/file/d/1YxtaPHvSEkEnUwldlODPoC0WE4KsfmOv/view?usp=sharing
+- Test Cases: https://drive.google.com/drive/folders/1xF0JyceIbiQHF_KkqiGLhRmj1lEmqYtm?usp=sharing
+
+### 3. Project Guildeline Details
+This section helps the user/instructor understand the process of running the project code. Please read and follow instructions strictly!
+1. Clone the project:<br/>
+In the project, "ss1-final-project" is the major branch, so you can do following way to clone the code from this branch:
+`git clone -b ss1-final-project https://github.com/hoa2000kxpt/CompanyBlog.git`
+2. Run the project:<br/> 
+You can start the project by typing `python manage.py runserver` in the command prompt.
+
+***Note: Please make sure that you installed Python and Django in your computer before.***
+
+
---
+++ OneDrive/Desktop/hoa - Copy chưa cps thông báp - Copy/users/views.py
@@ -0,0 +1,89 @@
+# from django.dispatch.dispatcher import receiver
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.models import User
+# from django.urls import conf
+# from django.db.models import Q
+from .models import Profile
+# from django.contrib.auth.forms import CustomUserCreationForm, ProfileForm, SkillForm, MessageForm
+from .forms import CustomUserCreationForm
+
+def loginUser(request):
+    page = 'login'
+
+    # if request.user.is_authenticated:
+    #     return redirect('login')
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'Username does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            # return redirect(request.GET['next'] if 'next' in request.GET else 'account')
+            return redirect('starting-page')
+        else:
+            messages.error(request, 'Username OR password is incorrect')
+
+    # return render(request, 'users/login_register.html')
+    return render(request, 'users/login_register.html')
+
+def logoutUser(request):
+    logout(request)
+    messages.info(request, 'User was logged out!')
+    return redirect('login')
+
+def profiles(request):
+    # profiles, search_query = searchProfiles(request)
+
+    # custom_range, profiles = paginateProfiles(request, profiles, 3)
+    # context = {'profiles': profiles, 'search_query': search_query,
+    #            'custom_range': custom_range}
+
+    # profiles=Profile.objects.all()
+    # context={'profiles': profiles}
+
+    return render(request, 'users/profiles.html')
+
+def userProfile(request, pk):
+    profile = Profile.objects.get(id=pk)
+
+    topSkills = profile.skill_set.exclude(description__exact="")
+    otherSkills = profile.skill_set.filter(description="")
+
+    context = {'profile': profile, 'topSkills': topSkills,
+               "otherSkills": otherSkills}
+    return render(request, 'users/user-profile.html', context)
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            messages.success(request, 'User account was created!')
+
+            login(request, user)
+            return redirect('starting-page') #edit this
+
+        else:
+            messages.success(
+                request, 'An error has occurred during registration')
+
+    context = {'page': page, 'form': form}
+    return render(request, 'users/login_register.html', context)
+
